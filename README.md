a3fd3f3a0e9c58ad8ed44966e5683df81b49b1b9
Steve <anil.stephen66@gmail.com>
2020-03-10 20:54:42 +0530

    Updated files

--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 
 ## NPM Commands
 
-* touch gulpfile.js -> Creates gulpfile.js(All the task logic goes here)
-* npm init -> Creates package.json fie
-* npm install -> Install's the node modules as mentioned in the package.json file
-* npm install --save-dev package-name( ex: npm install --save-dev package-name )  -> Install's gulp package in dev dependency
-* Similarly install all the packages for your project
+1. touch gulpfile.js -> Creates gulpfile.js(All the task logic goes here)
+2. npm init -> Creates package.json fie
+3. npm install -> Install's the node modules as mentioned in the package.json file[Use only if package.json file is available]
+4. npm install --save-dev package-name( ex: npm install --save-dev package-name )  -> Install's gulp package in dev dependency
+5. Similarly install all the packages for your project
 
 ## Commands for using the packages
 
--- gulpfile.js
+++ gulpfile.js
@@ -5,10 +5,21 @@ var sass = require( 'gulp-sass' );
 var autoprefixer = require( 'gulp-autoprefixer' );
 var sourcemaps = require( 'gulp-sourcemaps' );
 
-// File Path
+// File path for styles
 var styleSrc   = './src/scss/style.scss';
 var styleDest  = './dist/css/';
 
+// File path for js
+var jsSrc   = './src/js/script.js';
+var jsDest  = './dist/js/';
+
+//Watch file paths
+var watchStyle = './src/scss/**/*.scss';
+var watchJs = './src/js/**/*.js';
+
+
+
+// style task
 gulp.task('style',function(){
 
   gulp.src( styleSrc )
@@ -27,3 +38,29 @@ gulp.task('style',function(){
       .pipe( gulp.dest( styleDest ) );
 
 });
+
+// js task
+gulp.task('js',function(){
+  gulp.src( jsSrc )
+  .pipe( gulp.dest( jsDest ) );
+});
+/**
+ * start-> fire multiple task
+ * gulp.series() -> takes in all task names
+ * Code below can run multiple tasks ( task names are inside array )
+ */
+gulp.task( 'start', gulp.series( 'style', 'js' ) );
+
+
+/**
+ * watch -> Get's triggered when any of the file is updated/deleted/added
+ * watch then compiles all the files
+ */
+// gulp.task( 'watch', gulp.series( 'style', 'js' ), function(){
+  /**
+   * watchStyle -> File location
+   * ['style'] -> Task name
+   */
+//   gulp.watch( watchStyle, ['style'] );
+//   gulp.watch( watchJs, ['js'] );
+// });
